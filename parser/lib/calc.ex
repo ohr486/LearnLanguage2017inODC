873c84d7d08f6759b36bcd71d817321243e12031
ohr486 <ohr486@gmail.com>
2017-08-18 00:41:41 +0900

    わかりやすく順番整理

--- parser/lib/calc.ex
+++ parser/lib/calc.ex
@@ -1,31 +1,16 @@
 defmodule Calc do
   alias Parser, as: P
 
-  def number do
-    P._or(
-      P.str("0"),
-      P.reg(~r/[1-9][0-9]*/)
+  def expression, do: P.eval(fn -> additive() end)
+
+  def additive do
+    P.comb(
+      multitive(),
+      P._or(
+        P.map(P.str("+"), fn _ -> &(&1 + &2) end),
+        P.map(P.str("-"), fn _ -> &(&1 - &2) end)
+      )
     )
-    |> P.map(&to_number/1)
-  end
-
-  defp to_number(result) do
-    result |> Integer.parse |> elem(0)
-  end
-
-  def primary do
-    P._or(
-      P.map(
-        P.str("(") |> P.seq(expression()) |> P.seq(P.str(")")),
-        &strip_parentheses/1
-      ),
-      number()
-    )
-  end
-
-  defp strip_parentheses(result) do
-    {{"(", exp}, ")"} = result
-    exp
   end
 
   def multitive do
@@ -38,15 +23,28 @@ defmodule Calc do
     )
   end
 
-  def additive do
-    P.comb(
-      multitive(),
-      P._or(
-        P.map(P.str("+"), fn _ -> &(&1 + &2) end),
-        P.map(P.str("-"), fn _ -> &(&1 - &2) end)
-      )
+  def primary do
+    P._or(
+      P.map(
+        P.str("(") |> P.seq(expression()) |> P.seq(P.str(")")),
+        &strip_parentheses/1
+      ),
+      number()
     )
   end
+  defp strip_parentheses(result) do
+    {{"(", exp}, ")"} = result
+    exp
+  end
 
-  def expression, do: P.eval(fn -> additive() end)
+  def number do
+    P._or(
+      P.str("0"),
+      P.reg(~r/[1-9][0-9]*/)
+    )
+    |> P.map(&to_number/1)
+  end
+  defp to_number(result) do
+    result |> Integer.parse |> elem(0)
+  end
 end
