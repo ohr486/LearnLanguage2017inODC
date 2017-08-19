ad206f5850c04e8aaccb38e72caf8ef51630d84c
ohr486 <ohr486@gmail.com>
2017-08-19 15:35:20 +0900

    add sample code

---
+++ sample/doc_sample.ex
@@ -0,0 +1,10 @@
+defmodule DocSample do
+  @moduledoc "これはモジュールドキュメントです"
+
+  @doc "これは関数aのドキュメントです"
+  def a, do: :a
+
+  @doc "これは関数bのドキュメントです"
+  def b, do: :b
+end
+
---
+++ sample/fast_fib.ex
@@ -0,0 +1,24 @@
+defmodule FastFib do
+  @moduledoc """
+    フィボナッチ数を計算
+    メモ化して高速化
+  """
+
+  @doc "フィボナッチ数を保存するAgentプロセスを起動"
+  def start_link do
+    Agent.start_link(fn -> %{0 => 0, 1 => 1} end, name: __MODULE__)
+  end
+
+  @doc nil
+  def calc(n) do
+    case Agent.get(__MODULE__, &Map.get(&1, n)) do
+      nil ->
+        # Agentに対象のフィボナッチ数が無ければ計算して追加
+        value = calc(n - 1) + calc(n - 2)
+        Agent.update(__MODULE__, &Map.put(&1, n, value))
+        value
+      value -> value
+    end
+  end
+end
+
